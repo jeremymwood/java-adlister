375d9bdde542be9c585f362deaaf7ed22a79114d
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-12 11:22:30 -0500

    1122-prducts-ex-working

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -7,5 +7,19 @@ public class Product {
         this.name = name;
         this.price = price;
     }
-    // getters and setters...
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
