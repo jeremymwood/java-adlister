37e04edef57dea41999befdac33d3911278434a0
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-13 21:06:38 -0500

    2106

---
+++ books_migration 2.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS books_example;
+
+USE books_example;
+
+CREATE TABLE books (
+    id int UNSIGNED AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO books(title, author) VALUES ('The Hunchback of Notre Dame', 'Victor Hugo'), ('Green Eggs and Ham', 'Dr. Seuss');
