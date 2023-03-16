12eaf462a815657591d25535ea5bae3de97536c8
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-16 09:19:56 -0500

    0909

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -1,5 +1,8 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,35 +23,17 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        User user = DaoFactory.getUsersDao().findByUsername(username);
 
-//        String sql = "SELECT * FROM products WHERE name LIKE ?";
-//        String searchTermWithWildcards = "%" + searchTerm + "%";
-//
-//        PreparedStatement stmt = connection.prepareStatement(sql);
-//        stmt.setString(1, searchTermWithWildcards);
-//
-//        ResultSet rs = stmt.executeQuery();
-//        while(rs.next()) {
-//            // do something with the search results
-//        }
-//
-//        boolean validAttempt = username.equals("admin") && password.equals("123password456");
-//
-//        if (validAttempt) {
-//            request.getSession().setAttribute("user", username);
-//            response.sendRedirect("/profile");
-//        } else {
-//            response.sendRedirect("/login");
-//        }
-//
-        // TODO: find a record in your database that matches the submitted password
-        // TODO: make sure we find a user with that username
-        // TODO: check the submitted password against what you have in your database
-        boolean validAttempt = false;
+        if (user == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+
+        boolean validAttempt = password.equals(user.getPassword());
 
         if (validAttempt) {
-            // TODO: store the logged in user object in the session, instead of just the username
-            request.getSession().setAttribute("user", username);
+            request.getSession().setAttribute("user", user);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
--- src/main/webapp/WEB-INF/register.jsp
+++ src/main/webapp/WEB-INF/register.jsp
@@ -1,6 +1,4 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-
 <html>
 <head>
     <jsp:include page="/WEB-INF/partials/head.jsp">
@@ -11,10 +9,14 @@
     <jsp:include page="/WEB-INF/partials/navbar.jsp" />
     <div class="container">
         <h1>Please Log In</h1>
-        <form action="/login" method="POST">
+        <form action="/register" method="POST">
             <div class="form-group">
                 <label for="username">Username</label>
                 <input id="username" name="username" class="form-control" type="text">
+            </div>
+            <div class="form-group">
+                <label for="email">E-mail</label>
+                <input id="email" name="email" class="form-control" type="text">
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
