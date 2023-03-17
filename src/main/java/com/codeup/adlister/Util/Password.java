ccb8d6f6b05cb66ae5dd3c1ff5d94a3b074bdd55
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-03-16 19:35:15 -0500

    1935

--- src/main/java/com/codeup/adlister/Util/Password.java
+++ src/main/java/com/codeup/adlister/Util/Password.java
@@ -1,5 +1,4 @@
 package com.codeup.adlister.Util;
-
 import org.mindrot.jbcrypt.BCrypt;
 
 public class Password {
@@ -12,14 +11,4 @@ public class Password {
     public static boolean check(String password, String hash) {
         return BCrypt.checkpw(password, hash);
     }
-//
-//    public static void main(String[] args) {
-//        String pw = "password";
-//        System.out.println(Password.hash(pw));
-//        System.out.println(Password.hash(pw));
-//        System.out.println(Password.hash(pw));
-//        System.out.println(Password.hash(pw));
-//        String hash = "$2a$12$Vt9xJ9MTr7RmAVDP5fjHP.zkj4OttrSVgDYaPfxhUy.jeDutxoPee";
-//        System.out.println(Password.check(pw, hash));
-//    }
 }
--- src/main/webapp/WEB-INF/register.jsp
+++ src/main/webapp/WEB-INF/register.jsp
@@ -26,7 +26,7 @@
 <%--                <label for="confirm_password">Confirm password</label>--%>
 <%--                <input id="confirm_password" name="confirm_password" class="form-control" type="password">--%>
 <%--            </div>--%>
-            <input type="submit" class="btn btn-primary btn-block" value="Register">
+            <input type="submit" class="btn btn-primary btn-block w-100" value="Register">
         </form>
     </div>
 </body>
